8b8749a2b73f7374e63b4b089d463d579e8762f8
raikel <raikelbl@gmail.com>
2020-04-21 10:00:38 -0500

    Project updated

--- api/apps.py
+++ api/apps.py
@@ -1,10 +1,18 @@
+import logging
+
 from django.apps import AppConfig
+from django.conf import settings
+
+logger_name = settings.LOGGER_NAME
+logger = logging.getLogger(logger_name)
 
 
 class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        from . import signals
-        from .services import create_schemas
-        create_schemas()
+        try:
+            from .services import create_schemas
+            create_schemas()
+        except Exception as error:
+            logger.warning(error)
